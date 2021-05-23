2081a2f087e663a2a6e289ff7c64d643f636ff30
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-05-23 09:19:59 +0300

    fix reindex task handler

--- listener-log/src/main/java/com/rarible/ethereum/listener/log/ReindexTopicTaskHandler.kt
+++ listener-log/src/main/java/com/rarible/ethereum/listener/log/ReindexTopicTaskHandler.kt
@@ -1,17 +1,17 @@
 package com.rarible.ethereum.listener.log
 
 import com.rarible.core.task.TaskHandler
-import com.rarible.ethereum.listener.log.persist.BlockRepository
 import io.daonomic.rpc.domain.Word
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.reactive.asFlow
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
+import scalether.core.MonoEthereum
 
 @Component
 class ReindexTopicTaskHandler(
     private val logListenService: LogListenService,
-    private val blockRepository: BlockRepository
+    private val ethereum: MonoEthereum
 ) : TaskHandler<Long> {
 
     override val type: String
@@ -30,8 +30,7 @@ class ReindexTopicTaskHandler(
     }
 
     private fun fetchNormalBlockNumber() =
-        blockRepository.findFirstByIdAsc()
-            .map { it.id }
+        ethereum.ethBlockNumber().map { it.toLong() }
 
     companion object {
         const val TOPIC = "TOPIC"

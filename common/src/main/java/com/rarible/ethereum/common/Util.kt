5720375e49289c35cca0b7364945cfc348d1de69
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-05-27 13:30:17 +0300

    added some utils

--- common/src/main/java/com/rarible/ethereum/common/Util.kt
+++ common/src/main/java/com/rarible/ethereum/common/Util.kt
@@ -1,8 +1,13 @@
 package com.rarible.ethereum.common
 
+import io.daonomic.rpc.domain.Binary
 import io.daonomic.rpc.domain.Bytes
 import io.daonomic.rpc.domain.Word
+import org.apache.commons.lang3.RandomUtils
 import org.web3j.crypto.Hash
+import org.web3j.crypto.Keys
+import org.web3j.crypto.Sign
+import org.web3j.utils.Numeric
 import scalether.domain.Address
 import scalether.util.Hex
 import java.math.BigInteger
@@ -20,3 +25,18 @@ fun keccak256(bytes: ByteArray): Word = Word(Hash.sha3(bytes))
 fun keccak256(bytes: Bytes): Word = keccak256(bytes.bytes())
 
 fun keccak256(str: String): Word = keccak256(str.toByteArray(Charsets.US_ASCII))
+
+fun Sign.SignatureData.toBinary(): Binary = Binary.apply(this.r).add(this.s).add(byteArrayOf(this.v))
+
+fun generateNewKeys(): NewKeys {
+    val privateKey = Numeric.toBigInt(RandomUtils.nextBytes(32))
+    val publicKey = Sign.publicKeyFromPrivate(privateKey)
+    val signer = Address.apply(Keys.getAddressFromPrivateKey(privateKey))
+    return NewKeys(privateKey, publicKey, signer)
+}
+
+data class NewKeys(
+    val privateKey: BigInteger,
+    val publicKey: BigInteger,
+    val address: Address
+)

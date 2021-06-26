3aa9e7e06ef0b331a139a4c4d7a8687965a0648b
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-06-26 09:23:42 +0300

    AbstractPendingTransactionService implemented using coroutines

--- listener-log-service/src/main/java/com/rarible/ethereum/log/service/AbstractPendingTransactionService.kt
+++ listener-log-service/src/main/java/com/rarible/ethereum/log/service/AbstractPendingTransactionService.kt
@@ -1,18 +1,15 @@
 package com.rarible.ethereum.log.service
 
-import com.rarible.core.logging.LoggingUtils
 import com.rarible.ethereum.listener.log.domain.LogEvent
 import com.rarible.ethereum.log.LogEventsListener
 import com.rarible.ethereum.log.domain.TransactionDto
 import io.daonomic.rpc.domain.Binary
 import io.daonomic.rpc.domain.Word
+import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.reactive.awaitFirstOrNull
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.slf4j.Marker
 import org.springframework.dao.DuplicateKeyException
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toFlux
 import scalether.abi.Signature
 import scalether.domain.Address
 
@@ -22,35 +19,34 @@ abstract class AbstractPendingTransactionService(
 ) {
     protected val logger: Logger = LoggerFactory.getLogger(javaClass)
 
-    fun process(tx: TransactionDto): Flux<LogEvent> {
-        return LoggingUtils.withMarkerFlux { marker ->
-            val id = tx.input.slice(0, 4)
-            val data = tx.input.slice(4, tx.input.length())
-            process(marker, tx.hash, tx.from, tx.nonce, tx.to, id, data)
-                .flatMap {
-                    logEventService.save(it)
-                        .onErrorResume(DuplicateKeyException::class.java) { _ ->
-                            logger.warn(marker, "history already created")
-                            Mono.just(it)
-                        }
-                }
-                .collectList()
-                .flatMapMany { list ->
-                    listener.postProcessLogs(list)
-                        .thenMany(list.toFlux())
-                }
+    suspend fun process(tx: TransactionDto): List<LogEvent> {
+        val id = tx.input.slice(0, 4)
+        val data = tx.input.slice(4, tx.input.length())
+        val logs = process(tx.hash, tx.from, tx.nonce, tx.to, id, data)
+            .map { saveOrReturn(it) }
+            .toList()
+
+        listener.postProcessLogs(logs).awaitFirstOrNull()
+        return logs
+    }
+
+    private suspend fun saveOrReturn(log: LogEvent): LogEvent {
+        return try {
+            logEventService.save(log).awaitFirst()
+        } catch (e: DuplicateKeyException) {
+            logger.warn("history already created")
+            log
         }
     }
 
-    protected abstract fun process(
-        marker: Marker,
+    protected abstract suspend fun process(
         hash: Word,
         from: Address,
         nonce: Long,
         to: Address?,
         id: Binary,
         data: Binary
-    ) : Flux<LogEvent>
+    ) : List<LogEvent>
 
     protected fun <I> checkTx(id: Binary, data: Binary, signature: Signature<I, *>): I? {
         return if (id == signature.id()) {

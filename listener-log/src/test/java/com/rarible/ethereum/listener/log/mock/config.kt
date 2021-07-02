2f3ce971d571b6e24490eb95f3e5548017159ac7
sergey.anufriev <s.anufriev@rarible.com>
2021-07-02 11:33:16 +0700

    Extend ListenTransfersTest

--- listener-log/src/test/java/com/rarible/ethereum/listener/log/mock/config.kt
+++ listener-log/src/test/java/com/rarible/ethereum/listener/log/mock/config.kt
@@ -2,9 +2,41 @@ package com.rarible.ethereum.listener.log.mock
 
 import com.github.cloudyrock.spring.v5.EnableMongock
 import com.rarible.ethereum.listener.log.EnableLogListeners
+import com.rarible.contracts.test.erc20.TransferEvent
+import com.rarible.contracts.test.erc1155.TransferSingleEvent
+import com.rarible.ethereum.listener.log.OnLogEventListener
+import io.mockk.every
+import io.mockk.mockk
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Bean
+import reactor.core.publisher.Mono
 
 @EnableLogListeners(scanPackage = [TestLogConfiguration::class])
 @EnableMongock
 @EnableAutoConfiguration
-class TestLogConfiguration
+class TestLogConfiguration {
+    @Bean
+    fun onErc20TransferEventListener1() : OnLogEventListener {
+        return mockk {
+            every { topic } returns TransferEvent.id()
+            every { onLogEvent(any()) } returns Mono.empty()
+        }
+    }
+
+    @Bean
+    fun onErc20TransferEventListener2() : OnLogEventListener {
+        return mockk {
+            every { topic } returns TransferEvent.id()
+            every { onLogEvent(any()) } returns Mono.empty()
+        }
+    }
+
+    @Bean
+    fun onOtherEventListener() : OnLogEventListener {
+        return mockk {
+            every { topic } returns TransferSingleEvent.id()
+            every { onLogEvent(any()) } returns Mono.empty()
+        }
+    }
+}

75daeabf008708368d942f89429f22cf33a55b1d
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-10-02 20:47:17 +0300

    Add javadoc for LogEvent.

--- listener-log-domain/src/main/java/com/rarible/ethereum/listener/log/domain/LogEvent.kt
+++ listener-log-domain/src/main/java/com/rarible/ethereum/listener/log/domain/LogEvent.kt
@@ -9,17 +9,59 @@ import org.springframework.data.annotation.Version
 import scalether.domain.Address
 import java.time.Instant
 
+/**
+ * Log event emitted by a smart contract and subject for business processing.
+ * Such [LogEvent]s are filled with [data] created by [com.rarible.ethereum.listener.log.LogEventDescriptor]s.
+ * They are saved to dedicated repositories, from which are queries and processed by upper level code (such as reducing services).
+ */
 data class LogEvent(
+    /**
+     * Arbitrary business data associated with this log event. This is produced by `LogEventDescriptor`.
+     */
     val data: EventData,
+    /**
+     * Address of the smart contract that produced this log event.
+     */
     val address: Address,
+    /**
+     * ID of the log event as defined by Ethereum log (e.g. `keccak256("SomeLogEvent(address,unit256)")`).
+     */
     val topic: Word,
+    /**
+     * Hash of the transaction inside block #[blockNumber].
+     */
     val transactionHash: Word,
+    /**
+     * Status of the log event. Basically, only [LogEventStatus.CONFIRMED] log events impact business calculations.
+     */
     val status: LogEventStatus,
+    /**
+     * Hash of the block inside which this log event was produced.
+     */
     val blockHash: Word? = null,
+    /**
+     * Number of the block inside which this log event was produced.
+     */
     val blockNumber: Long? = null,
+    /**
+     * Index of this log event inside the whole block.
+     */
     val logIndex: Int? = null,
+    /**
+     * Secondary index of this log event among all logs produced by `LogEventDescriptor.convert` for the same log
+     * with exactly the same [blockNumber], [blockHash], [transactionHash], [logIndex] and [index].
+     * So this [minorLogIndex] is used to distinguish consuquent events.
+     */
     val minorLogIndex: Int,
+    /**
+     * Index of this log event inside the transaction in which it was produced.
+     * It is different from [logIndex] is that the [logIndex] is per-block but [index] is per-transaction.
+     * Also note that [logIndex] is a commonly used index (defined in Ethereum spec), which the [index] is calculated by our code.
+     */
     val index: Int,
+    /**
+     * Whether this log event should be considered for processing.
+     */
     val visible: Boolean = true,
     @Id
     override val id: ObjectId = ObjectId.get(),

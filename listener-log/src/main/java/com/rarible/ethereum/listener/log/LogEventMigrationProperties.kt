fbaa0d26d4425409f3a55bf4eb37d83574e33f52
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-10-27 11:05:02 +0300

    LogEvent index fix [RPN-1279]: create index containing the 'address' field.

---
+++ listener-log/src/main/java/com/rarible/ethereum/listener/log/LogEventMigrationProperties.kt
@@ -0,0 +1,9 @@
+package com.rarible.ethereum.listener.log
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+data class LogEventMigrationProperties(
+    @Value("\${createLogEventIndexContainingAddress:false}") val createLogEventIndexContainingAddress: Boolean
+)
--- listener-log/src/main/java/com/rarible/ethereum/listener/log/mongock/ChangeLog00001.kt
+++ listener-log/src/main/java/com/rarible/ethereum/listener/log/mongock/ChangeLog00001.kt
@@ -4,8 +4,10 @@ import com.github.cloudyrock.mongock.ChangeLog
 import com.github.cloudyrock.mongock.ChangeSet
 import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate
 import com.rarible.ethereum.listener.log.LogEventDescriptorHolder
+import com.rarible.ethereum.listener.log.LogEventMigrationProperties
 import io.changock.migration.api.annotations.NonLockGuarded
 import org.bson.Document
+import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.index.Index
 import org.springframework.data.mongodb.core.index.PartialIndexFilter
@@ -15,6 +17,8 @@ import org.springframework.data.mongodb.core.query.Update
 
 @ChangeLog(order = "00001")
 class ChangeLog00001 {
+
+    private val logger = LoggerFactory.getLogger(ChangeLog00001::class.java)
 
     @ChangeSet(id = "fillMinorLogIndex", order = "0000", author = "eugene")
     fun fillMinorLogIndex(template: MongockTemplate, @NonLockGuarded holder: LogEventDescriptorHolder) {
@@ -28,6 +32,35 @@ class ChangeLog00001 {
     fun ensureInitialIndexes(template: MongockTemplate, @NonLockGuarded holder: LogEventDescriptorHolder) {
         val collections = holder.list.map { it.collection }.toSet()
         collections.forEach { createInitialIndices(template, it) }
+    }
+
+    @ChangeSet(id = "createLogEventIndexContainingAddress", order = "00002", runAlways = true, author = "Patrikeev")
+    fun createLogEventIndexContainingAddress(
+        template: MongockTemplate,
+        @NonLockGuarded logEventMigrationProperties: LogEventMigrationProperties,
+        @NonLockGuarded holder: LogEventDescriptorHolder
+    ) {
+        if (!logEventMigrationProperties.createLogEventIndexContainingAddress) {
+            logger.info("Skip creation of the new log event index containing address")
+            return
+        }
+        val collections = holder.list.map { it.collection }.toSet()
+        collections.forEach {
+            val indexOps = template.indexOps(it)
+            indexOps.ensureIndex(
+                Index()
+                    .on("transactionHash", Sort.Direction.ASC)
+                    .on("topic", Sort.Direction.ASC)
+                    .on("address", Sort.Direction.ASC)
+                    .on("index", Sort.Direction.ASC)
+                    .on("minorLogIndex", Sort.Direction.ASC)
+                    .on("visible", Sort.Direction.ASC)
+                    .named("transactionHash_1_topic_1_address_1_index_1_minorLogIndex_1_visible_1")
+                    .background()
+                    .unique()
+                    .partial(PartialIndexFilter.of(Document("visible", true)))
+            )
+        }
     }
 
     private fun createInitialIndices(template: MongockTemplate, collection: String) {

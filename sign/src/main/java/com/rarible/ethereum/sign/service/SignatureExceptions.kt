43daf7138fc0fe566eb6975bcc44db57af53ae75
Artem Yakischik <a.yakischik@rarible.com>
2021-11-02 14:46:24 +0400

    RPN-1302 Same exception for signature validation cases

--- sign/src/main/java/com/rarible/ethereum/sign/service/SignatureExceptions.kt
+++ sign/src/main/java/com/rarible/ethereum/sign/service/SignatureExceptions.kt
@@ -2,4 +2,7 @@ package com.rarible.ethereum.sign.service
 
 class CheckSignatureException(message: String, cause: Exception) : RuntimeException(message, cause)
 
-class InvalidSignatureException(message: String, cause: Exception) : RuntimeException(message, cause)
+class InvalidSignatureException : RuntimeException {
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+}

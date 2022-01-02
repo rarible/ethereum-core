d3f6d375a7fc8c5823764b0b3a8848675c8081a9
Eugene Nacu <evgeny.nacu@gmail.com>
2022-01-02 07:50:48 +0300

    Release 1.4.11: add delay to check wrong hash task

--- listener-log/src/main/java/com/rarible/ethereum/listener/log/mongock/CheckWrongHashTaskHandler.kt
+++ listener-log/src/main/java/com/rarible/ethereum/listener/log/mongock/CheckWrongHashTaskHandler.kt
@@ -4,12 +4,14 @@ import com.rarible.core.task.TaskHandler
 import com.rarible.ethereum.listener.log.domain.BlockHead
 import com.rarible.ethereum.listener.log.domain.BlockStatus
 import com.rarible.ethereum.listener.log.persist.BlockRepository
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.awaitFirst
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import scalether.core.MonoEthereum
 
@@ -17,6 +19,7 @@ import scalether.core.MonoEthereum
 class CheckWrongHashTaskHandler(
     private val blockRepository: BlockRepository,
     private val ethereum: MonoEthereum,
+    @Value("ethereumCheckWrongHashDelay:10000") private val delay: Long
 ) : TaskHandler<Long> {
 
     override val type: String
@@ -36,6 +39,7 @@ class CheckWrongHashTaskHandler(
         if (bb.hash() != block.hash) {
             logger.info("Block has incorrect hash in the db: ${block.id}, old hash: ${block.hash}, new hash: ${bb.hash()}")
             blockRepository.save(block.copy(status = BlockStatus.ERROR, hash = bb.hash(), timestamp = bb.timestamp().toLong()))
+            delay(delay)
         }
     }
 

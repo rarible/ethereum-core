2b6687301eea3d5393f89c030638e1d43fe43d5c
Sergey Ratushnyak <s.ratushnyak@rarible.com>
2022-05-09 02:18:14 +0300

    feat: ALPHA-415 add sync fields to etherium for activity

--- listener-log/src/main/java/com/rarible/ethereum/listener/log/persist/LogEventRepository.kt
+++ listener-log/src/main/java/com/rarible/ethereum/listener/log/persist/LogEventRepository.kt
@@ -78,7 +78,7 @@ class LogEventRepository(
                     logger.info(marker, "reverting $it")
                     it.copy(status = LogEventStatus.REVERTED, visible = false)
                 }
-                .flatMap { mongo.save(it.withDbUpdated(), collection) }
+                .flatMap { save(collection, it) }
         }
     }
 

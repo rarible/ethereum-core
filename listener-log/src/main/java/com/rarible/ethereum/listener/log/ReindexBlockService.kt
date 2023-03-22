a598d9876b463499f02d0fa3c123d54037f80c3b
sanufriev <s.anufriev@rarible.com>
2023-03-22 12:22:23 +0700

    faet: Improve reindex

--- listener-log/src/main/java/com/rarible/ethereum/listener/log/ReindexBlockService.kt
+++ listener-log/src/main/java/com/rarible/ethereum/listener/log/ReindexBlockService.kt
@@ -9,14 +9,18 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.reactor.mono
+import kotlinx.coroutines.time.withTimeout
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.time.Duration
 
 @Service
 class ReindexBlockService(
     private val blockRepository: BlockRepository,
     private val logListenService: LogListenService,
-    @Value("\${ethereumReindexBatchSize:5}") private val ethereumReindexBatchSize: Int
+    @Value("\${ethereumReindexBatchSize:5}") private val ethereumReindexBatchSize: Int,
+    @Value("\${ethereumReindexBlockTimeout:5000}") private val ethereumReindexBlockTimeout: Long,
 ) {
     fun indexPendingBlocks() = mono {
         val pending = getBlocks(BlockStatus.PENDING)
@@ -42,10 +46,18 @@ class ReindexBlockService(
     }
 
     private suspend fun reindexBlock(block: BlockHead) {
-        logListenService.reindexBlock(block).awaitFirstOrNull()
+        try {
+            withTimeout(Duration.ofMillis(ethereumReindexBlockTimeout)) {
+                logListenService.reindexBlock(block).awaitFirstOrNull()
+            }
+        } catch (ex: Throwable) {
+            logger.error("Can't reindex block ${block.id}", ex)
+        }
     }
 
     private suspend fun getBlocks(status: BlockStatus): List<BlockHead> {
         return blockRepository.findByStatus(status).collectList().awaitSingle()
     }
+
+    private val logger = LoggerFactory.getLogger(ReindexBlockService::class.java)
 }

879f4127fc1b27f7ec9dc888834f63e57f586cc2
Dmitry Saraykin <d.saraykin@rarible.com>
2024-03-27 17:32:55 +0300

    feat(PT-4394): remove websockets. fix test

--- listener-log/src/test/java/com/rarible/ethereum/listener/log/BlockRepositoryTest.kt
+++ listener-log/src/test/java/com/rarible/ethereum/listener/log/BlockRepositoryTest.kt
@@ -16,23 +16,25 @@ class BlockRepositoryTest : AbstractIntegrationTest() {
 
     @Test
     fun findBlocksWorks() = runBlocking<Unit> {
-        mongo.save(BlockHead(100, WordFactory.create(), 1)).awaitFirst()
-        mongo.save(BlockHead(101, WordFactory.create(), 1)).awaitFirst()
+        val block1 = BlockHead(100, WordFactory.create(), 1)
+        val block2 = BlockHead(101, WordFactory.create(), 1)
+        mongo.save(block1).awaitFirst()
+        mongo.save(block2).awaitFirst()
 
         assertThat(
-            blockRepository.findBlocks(100, 101).collectList().awaitFirst()
-        ).hasSize(1)
+            blockRepository.findBlocks(100, 101).collectList().awaitFirst().map { it.id }
+        ).containsExactly(block1.id)
 
         assertThat(
-            blockRepository.findBlocks(null, null).collectList().awaitFirst()
-        ).hasSize(2)
+            blockRepository.findBlocks(null, null).collectList().awaitFirst().map { it.id }
+        ).contains(block1.id, block2.id)
 
         assertThat(
-            blockRepository.findBlocks(101, null).collectList().awaitFirst()
-        ).hasSize(1)
+            blockRepository.findBlocks(101, null).collectList().awaitFirst().map { it.id }
+        ).contains(block2.id)
 
         assertThat(
-            blockRepository.findBlocks(null, 101).collectList().awaitFirst()
-        ).hasSize(1)
+            blockRepository.findBlocks(null, 101).collectList().awaitFirst().map { it.id }
+        ).contains(block1.id)
     }
 }

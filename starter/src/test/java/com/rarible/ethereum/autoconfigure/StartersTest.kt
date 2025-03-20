bdd7cacbd8ef3c37aebf0b3a22e6179449b9f24d
Igor Baltiyskiy <i.baltiyskiy@rarible.com>
2025-03-20 09:12:27 +0100

    PT-7127 Rename file to application.yaml

--- starter/src/test/java/com/rarible/ethereum/autoconfigure/StartersTest.kt
+++ starter/src/test/java/com/rarible/ethereum/autoconfigure/StartersTest.kt
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Import
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
@@ -21,7 +20,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
         "rarible.ethereum.parity.httpUrl=localhost:6543"
     ]
 )
-@ActiveProfiles("core")
 @Import(StartersTest.Configuration::class)
 @EnableAutoConfiguration
 class StartersTest {
